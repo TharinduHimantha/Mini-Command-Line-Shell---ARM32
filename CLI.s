1c5948a3c878d334b05fee5b6473b8ff99b0dcb7
Tharindu Himantha <tharinduhimantha2003@gmail.com>
2025-07-17 14:02:46 +0530

    Basic settings
    The basic CLI prompt implemented with proper sections in the program. Still dont have an exit mechanism. Might be upgraded to use sys_read and sys_write rather than scanf, printf in the future

---
+++ CLI.s
@@ -0,0 +1,41 @@
+@ ARM32 CLI
+
+.data @ known strings and variables
+
+cli_prompt:		.asciz "ARM32_cli> "
+input_format:   .asciz "%s"
+
+
+.bss		@ for the input buffer
+
+buffer: .space 200
+
+
+
+@ All the codes
+.text
+
+@ write all the functions here
+
+
+.global main
+
+@main loop
+main:
+
+shell_loop:
+
+	@ printing the prompt
+	ldr r0, =cli_prompt
+	bl printf
+
+	@ taking inputs
+	ldr r0, =input_format
+	ldr r1, =buffer
+	bl scanf
+
+	@ repeating
+	b shell_loop
+
+exit:
+
