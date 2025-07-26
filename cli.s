e1b3eae14a36dc00984915dbdd2b9c0c607a9d37
Tharindu Himantha <tharinduhimantha2003@gmail.com>
2025-07-27 03:44:22 +0530

    Function added

--- cli.s
+++ cli.s
@@ -6,6 +6,8 @@
 @ --------------------------------------------
 
 .data			@@ known strings and variables +++++++++++++++++++++++++++++++++++++++++++++++++++++
+__data_start:
+
 
 	cli_prompt:		.asciz "ARM32_cli> "
 	input_format:   .asciz "%49[^\n]"		@ read upto 49 char or '\n' is met
@@ -36,22 +38,46 @@
 	encrypt_format_full: .asciz "encrypt %d %s"
 	encrypt_usage:      .asciz "Usage: encrypt <shift> <text>\n"
 
+    uptime_cmd:  .asciz "performance"			@ command to trigger show_performance()
+	uptime_msg:  .asciz "Uptime: %02d:%02d:%02d\n"		@to show uptime
+
+	@ CPU time showing format
+	cpu_msg: .asciz "CPU Time:\n\tApplication: %d.%06ds, \n\tSystem: %d.%06ds\n"
+
+											@ idle time showing format()
+	idle_time: .asciz "\nIdle Time: %02d:%02d:%02d\n"
+	idle_precent: .asciz "Idle Precentage: %02d.%d \%\n\n"
+	const_value:  .word 1000000				@ Store the constant value in memory
+
+	@ memory usage showing format strings
+	total_size_: .asciz "Total memory used: %d KB\n"
+	segment_size_msg: .asciz "Segment sizes\n"
+	text_size_:  .asciz "\t.text  : %d bytes\n"
+	data_size_:  .asciz "\t.data  : %d bytes\n"
+	bss_size_:   .asciz "\t.bss   : %d bytes\n"
+
+
 @just for testing, clear up afterwards
 	test_input: .asciz "You entered : %s\n"	
 
-
+ __data_end:
 
 
 .bss			@@ for memory allocations +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+__bss_start:
 	buffer: .space 50					@to take user inputs
+	start_time: .space 4				@ to store start time
+	rusage_buf: .space 96				@ enough space for cpu info
 
-
+__bss_end:
 
 
 
 @@ All the codes and instructions ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 .text
+__text_start:
+
 
 .global main
 
@@ -100,6 +126,204 @@ clear_string_buffer:
 	@ buffer is reset
 
 	pop {pc}
+
+
+
+
+
+show_performance:
+
+
+
+	push {lr}
+
+	@@ for UpTime
+    bl get_time					@ get current time (r0 contains it)
+
+    @ subtract start_time from current_time
+    ldr r2, =start_time
+    ldr r2, [r2]				@ loading stored start_time
+
+    sub r0, r0, r2				@ r0 = uptime in seconds
+	mov r9, r0					@ copied for future use
+
+    @ convert r0 to hh:mm:ss
+    bl time_format				@ result: r0=hh, r1=mm, r2=ss
+
+    @ print uptime
+    mov r3, r2					@ r2 = ss
+    mov r2, r1					@ r1 = mm
+    mov r1, r0					@ r0 = hh
+	ldr r0, =uptime_msg
+    bl printf					@ printing Uptime
+
+
+	@@ CPU time for application
+	ldr r0, =0					@ current process as an argument(0)
+    ldr r1, =rusage_buf     
+    mov r7, #77					@ syscall number for getrusage
+    svc #0						@ software inturrupt
+
+								@ Extracting values from rusage_buf
+    ldr r5, =rusage_buf
+    ldr r1, [r5]				@ CPU time for application
+    ldr r2, [r5, #4]			@ decimal part (microsec)
+    ldr r3, [r5, #8]			@ CPU time for sys calls
+    ldr r4, [r5, #12]			@ decimal part (microsec)
+	ldr r10, [r5, #16]			@ Total memory used
+	ldr r0, =cpu_msg
+    bl printf					@ printing CPU times
+
+
+
+	@@ Idle time
+	ldr r8, =const_value		@Load the address of the constant
+	ldr r8, [r8] 
+
+
+	mul r5, r1, r8				@ CPU time appl in microsec
+	mul r6, r3, r8				@ CPU time sys in microsec
+
+	add r5, r5, r2				
+	add r6, r6, r4
+	add r5, r5, r6				@ Total CPU time
+
+	mul r6, r9, r8				@ UpTime in microsec
+
+	sub r5, r6, r5				@ Idle time in microsec = UpTime - Total CPU time
+
+	mov r0, r5
+	mov r1, r8
+	bl divider					@ idle time in seconds
+	mov r4, r0					@ r4 <- Idle time in seconds
+	bl time_format				@ Converting to correct format
+	push {r0,r1,r2}				@ Storing idle time for easiness
+
+	ldr r0, =idle_time	
+	pop {r1,r2,r3}				@ Restoring idle time
+    bl printf					@ Printing Idle time
+
+	mov r2,#100					
+	mov r1, r9					@ r1 <- uptime
+	mul r0, r4, r2				@ r0 <- idle time *100
+
+	bl divider					@ r0 = (idle time/uptime) *100
+	mov r2, r1
+	mov r1, r0
+	ldr r0, =idle_precent		
+	bl printf					@ printing idle precentage
+
+	@@ Memory usage
+	ldr r0, =total_size_
+	mov r1, r10					@ previously stored total memory size
+	bl printf
+	bl section_size
+
+    pop {pc}
+
+
+section_size:
+    push {lr}
+
+	@ the size of each segment in the program
+	ldr r0, =segment_size_msg
+	bl printf
+
+    @ for .text section
+    ldr r0, =__text_end
+    ldr r1, =__text_start
+    sub r1, r0, r1             @ r1 = .text section size
+    ldr r0, =text_size_
+    bl printf
+
+    @ for .data section 
+    ldr r0, =__data_end
+    ldr r1, =__data_start
+    sub r1, r0, r1             @ r1 = .data section size
+    ldr r0, =data_size_
+    bl printf
+
+    @ for .bss section
+    ldr r0, =__bss_end
+    ldr r1, =__bss_start
+    sub r1, r0, r1             @ r2 = bss size
+    ldr r0, =bss_size_
+    bl printf
+
+
+    pop {pc}
+
+
+get_time:
+
+@ ------------------------------------------
+@ Function: get_time
+@ Return value: r0 <- current time in Unix Timestamp
+@ Description: Do a system call, returns current time
+@ System call time() invoking and software intuurupt happens
+@ ------------------------------------------
+
+	@ Get current time
+    mov r0, #0            @ NULL argument to time()
+    mov r7, #13           @ syscall for time() in Linux
+    svc #0				  @ software inturrupt and invoke kernel
+	bx lr				  @ return with r0
+
+
+
+time_format:
+
+@ -----------------------------------------------------
+@ Function: time_format
+@ input:  r0 = total seconds
+@ output: r0 = hours, r1 = minutes, r2 = seconds
+@ Description: Convert given time of seconds to hours, minutes, seconds
+@ ------------------------------------------------------
+
+	push {lr}
+    mov r1, #3600
+	bl divider				@ total hours
+	mov r3, r0				@ r3 = hours = answer
+	mov r0, r1				@ r0 remaining seconds
+
+	mov r1, #60
+	bl divider				@ total minutes
+
+	mov r2, r1				@ r2 = ss
+	mov r1, r0				@ r1 = min
+	mov r0, r3				@ r0 = hh
+
+    pop {pc}
+
+
+divider:
+
+@ ------------------------------------------
+@ Function: divider
+@ input:  r0 = dividend, r1 = divisor
+@ output: r0 = answer, r1 = remainder
+@ Description: divide number in ro by r1
+@ Return the quotient and remainder
+@ ------------------------------------------
+
+
+	push {r2,lr}		@ save the previous value
+
+	mov r2,#0			@ 
+	while_loop:
+		cmp r0, r1
+		blt return_divider
+
+		sub r0, r1
+		add r2, r2, #1
+		b while_loop
+
+
+	return_divider:
+	mov r1, r0				@ r1 = remainder
+	mov r0, r2				@ r0 = answer
+	pop {r2,pc}
+
 
 
 hello:
@@ -194,7 +418,7 @@ parse_shift_digits:
     cmp r0, #0              @ Check for null terminator
     beq encrypt_error
     
-    @ Check if it's a digit
+    @ Check if it''s a digit
     cmp r0, #'0'
     blt encrypt_error
     cmp r0, #'9'
@@ -398,3 +622,4 @@ skip_command:
     b shell_loop
 
 exit:
+__text_end:
